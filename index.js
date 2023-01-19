3477f7961488290185d19e8d56ae61d0837d9f4f
Shikhar <sm.shikharmaheshwari@gmail.com>
2023-01-20 01:02:06 +0530

    config

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    console.log('hii')
+    res.send(login.html)
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
